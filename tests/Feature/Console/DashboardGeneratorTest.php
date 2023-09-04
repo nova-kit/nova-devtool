8177e0a86fcf7f062b847f20aa79aa48e6dc0a68
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-04 21:33:35 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/Console/DashboardGeneratorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NovaKit\NovaDevTool\Tests\Feature\Console;
+
+use Orchestra\Canvas\Core\Testing\TestCase;
+use Orchestra\Testbench\Concerns\WithWorkbench;
+
+class DashboardGeneratorTest extends TestCase
+{
+    use WithWorkbench;
+
+    /**
+     * Stubs files.
+     *
+     * @var array<int, string>|null
+     */
+    protected $files = [
+        'app/Nova/Dashboards/Main.php',
+        'app/Nova/Dashboards/Post.php',
+    ];
+
+    /** @test */
+    public function it_can_generate_dashboard_file()
+    {
+        $this->artisan('nova:dashboard', ['name' => 'Post'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Nova\Dashboards;',
+            'use Laravel\Nova\Dashboard;',
+            'class Post extends Dashboard',
+        ], 'app/Nova/Dashboards/Post.php');
+    }
+
+
+    /** @test */
+    public function it_can_generate_the_main_dashboard_file()
+    {
+        $this->artisan('nova:dashboard', ['name' => 'Main'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Nova\Dashboards;',
+            'use Laravel\Nova\Dashboards\Main as Dashboard;',
+            'class Main extends Dashboard',
+        ], 'app/Nova/Dashboards/Main.php');
+    }
+}
